c5368297dced1df5b7a5aec16b787344f9bd20a6
이진유 <22yuu@naver.com>
2022-02-09 23:29:34 +0900

    8.5 Home Screen part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,10 @@
+import { useQuery } from "react-query";
+import { getMovies } from "../api";
+
 function Home() {
+  const { data, isLoading } = useQuery(["movies", "now_playing"], getMovies);
+  console.log(data, isLoading);
+
   return <div style={{ backgroundColor: "white", height: "200vh" }}></div>;
 }
 export default Home;
