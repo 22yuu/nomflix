fa50b10b6a164c6902015fdd5fcfbd39c37cfbbf
이진유 <22yuu@naver.com>
2022-02-09 23:52:29 +0900

    8.6 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "8fb08141aeae3e58ef9474455567a379";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
